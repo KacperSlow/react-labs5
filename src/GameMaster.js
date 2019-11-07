9828c32e171522bcf9d8e8ad48e5b5245675efc8
KacperSlow <k.slowikowski@student.mini.pw.edu.pl>
2019-11-07 12:23:38 +0100

    Lab5Ex2

--- src/GameMaster.js
+++ src/GameMaster.js
@@ -3,20 +3,34 @@ import PlayerOne from './PlayerOne'
 import PlayerTwo from './PlayerTwo'
 
 class GameMaster extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = { 
+      playerOneName:"" ,
+      playerTwoName:""
+  }
+    this.nameOneChangeHandler = this.nameOneChangeHandler.bind(this);
+    this.nameTwoChangeHandler = this.nameTwoChangeHandler.bind(this);
+  }
+  nameOneChangeHandler(e){
+    this.setState({playerOneName: e.target.value})
+  }
+  nameTwoChangeHandler(e){
+    this.setState({playerTwoName: e.target.value})
+  }
   render() {
       return (
           <div >
             <div>
-              <PlayerOne />
+              <PlayerOne name= {this.state.playerOneName} />
               <br/>
-              <PlayerTwo />
+              <PlayerTwo name= {this.state.playerTwoName}/>
             </div>
             <br/>
             <div>
-              <label>Set Player One name</label> <input type="text"></input>
+              <label>Set Player One name</label> <input type="text" onChange={this.nameOneChangeHandler}></input>
               <br/><br/>
-              <label>Set Player Two name</label> <input type="text"></input>
+              <label>Set Player Two name</label> <input type="text" onChange={this.nameTwoChangeHandler}></input>
             </div>
           </div>
           
--- src/PlayerTwo.js
+++ src/PlayerTwo.js
@@ -5,7 +5,7 @@ function PlayerTwo(props) {
     return (
         <div style={{border:'3px solid black', position: "relative", padding:"10px"}}>
             <div><b>Player Two</b></div>
-            <label>Name: </label>
+            <label>Name: {props.name} </label>
             <br/>
             <label>Played number of times:</label>
             <button style={{position: "absolute", right: "10px", bottom:"5px"}}>
